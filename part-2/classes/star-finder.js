a154eff733b50e43bb638205273ea6cfc74cf290
Tim Douglas <tim.douglas@coopdigital.co.uk>
2020-01-05 18:31:43 +0000

    Finished returning true if a letter surrounded by *

--- part-2/classes/star-finder.js
+++ part-2/classes/star-finder.js
@@ -1,7 +1,11 @@
 function StarFinder() {}
 
 StarFinder.prototype.starredLetters = function (originalString) {
-  
+    if(originalString.match(/\*[a-zA-Z]\*/)) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 module.exports = StarFinder;
--- part-2/tests/test-starred-string.js
+++ part-2/tests/test-starred-string.js
@@ -5,7 +5,15 @@ const StarFinder = require('../classes/star-finder.js');
 describe('StarredStrings', function() {
   it('Should return true', function() {
     var starFinder = new StarFinder([]);
+    var input = "*a*%=3=*";
 
-    expect(starFinder.starredLetters(input)).to.equal(result);
+    expect(starFinder.starredLetters(input)).to.be.true;
+  })
+
+  it('Should return false', function() {
+    var starFinder = new StarFinder([]);
+    var input = "%*3*b%3";
+
+    expect(starFinder.starredLetters(input)).to.be.false;
   })
 });
